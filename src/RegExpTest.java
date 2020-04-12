b9b0de3af4036234f46457be814b856c077aee59
IhorKohut <ih.kohut@gmail.com>
2020-04-12 23:11:52 +0300

    Regexp homework

--- src/RegExpTest.java
+++ src/RegExpTest.java
@@ -17,8 +17,11 @@ public class RegExpTest {
         {"http://192.168.1.1/index.html", "192.168.1.1"},
         {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
         {"IP could not have negative numbers like -1.-2.-3.-4", ""},
-        {" Invalid IP: 300.300.300.300!", ""}
-        // TODO add 2 more test data here
+        {" Invalid IP: 300.300.300.300!", ""},
+        {"Valid IP: 100.100.100.100!", "100.100.100.100"},
+        {"Invalid IP: 253,0,0,0", ""},
+        {"Invalid IP: 168\\.98\\.86\\.1", ""},
+        {"Invalid IP: 168 98 86 1", ""}
     };
   }
 
